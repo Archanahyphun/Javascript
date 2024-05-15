71a4741ec5d7046df3e02c7808e9e313f3f29c78
Archanahyphun <167953987+Archanahyphun@users.noreply.github.com>
2024-05-15 05:42:55 +0000

    datatype

--- basic/datatype.js
+++ basic/datatype.js
@@ -1,12 +1,25 @@
 "use strict";  // treate all js code as newer version JS
-
 // alert('hellow') // We are using NODEJS not browser
-
-
-
-
 console.log("Test");
 console.group("Archana");
 console.log(3*3);
  console.group("Archana");
+ console.log(typeof null) // to know type of value
+ console.log(typeof undefined)
+ console.log(typeof "test")
+
+ let name ="Archana"
+ let age = 30
+ let loggedin = false
+ let state = null 
+
+ //  let state;  is undefined (value is not defined)
+ //  let state = null    standalone value
+ // Number => 2 to power 53
+ // bigint => (used for large number)
+ //string => ""
+ // boolean => true/false
+ // null  => standalone value
+ // undefined => 
+// Object 
 
