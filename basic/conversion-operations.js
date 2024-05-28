696dfa251a701dfc83796a41e3aae292ae8e9666
Archanahyphun <167953987+Archanahyphun@users.noreply.github.com>
2024-05-28 12:05:29 +0000

    conversion

---
+++ basic/conversion-operations.js
@@ -0,0 +1,14 @@
+let score = 33
+let str = "33aaa"
+let nll = null
+
+let valueInNumber = Number(str)
+
+console.log(typeof score);
+console.log(typeof (score));
+console.log(typeof str);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+console.log(typeof  nll);
+console.log(nll);
+// const(score) = req.body;
